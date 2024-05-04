33c0f05a07436cf6874d3b0d97f23cd5a05515de
Jniemel <juho_n@live.fi>
2024-05-04 23:27:27 +0300

    Recruiting characters to team now works correctly.

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,9 +1,13 @@
 /* eslint-disable camelcase */
 import { validationResult } from 'express-validator';
+import mongoose from 'mongoose';
 import { handleCatchErr, handleValErr } from '../utils/errorHandling.js';
 import GameState from '../models/gameStateModel.js';
+import { characterSchema } from '../models/characterModel.js';
 
-export async function getRecruits() {
+const Character = mongoose.model('Character', characterSchema);
+
+export async function new_get() {
   return null;
 }
 
@@ -13,11 +17,24 @@ export async function buy_post(req, res) {
     if (!valErrors.isEmpty()) {
       return handleValErr(res, valErrors);
     }
+    const { recruitee, price, _id } = req.body.recruit;
+    // create new character according to the new recruit
+    const character = new Character({
+      name: recruitee.name,
+      age: recruitee.age,
+      class: recruitee.class,
+      stats: recruitee.stats,
+      cssColor: recruitee.cssColor,
+    });
+    // find gameState and push the new character into the team
     const state = await GameState.findOne({ owner: req.username });
-    state.playerTeam.champs.push(req.body.char);
-    state.save();
-
-    return res.status(200); // .json({ user: user._id });
+    state.playerTeam.champs.push(character);
+    // set new balance
+    state.playerTeam.money -= price;
+    // remove the recruited character from recruit pool
+    state.recruitment.id(_id).deleteOne();
+    await state.save();
+    return res.status(200).end();
   } catch (err) {
     return handleCatchErr(res, err);
   }
--- utils/validation.js
+++ utils/validation.js
@@ -35,7 +35,7 @@ export const validateLogIn = [
 ];
 
 export const validateChamp = [
-  body('char.name' /* 'validation failed: name' */)
+  body('recruit.recruitee.name', 'validation failed: name')
     .trim()
     .isLength({ min: 5, max: 12 })
     .withMessage('here')
@@ -44,23 +44,71 @@ export const validateChamp = [
     .isAlpha()
     .withMessage('here3')
     .escape(),
-  body('char.age', 'validation failed: age')
+  body('recruit.recruitee.age', 'validation failed: age')
     .trim()
     .not()
     .isEmpty()
     .isNumeric()
     .escape(),
-  body('char.class', 'validation failed: class')
+  body('recruit.recruitee.class', 'validation failed: class')
     .trim()
     .isLength({ min: 1, max: 25 })
     .isString()
     .isAlpha()
     .escape(),
-  body('char.cssColor', 'validation failed: cssColor')
+  body('recruit.recruitee.cssColor', 'validation failed: cssColor')
     .trim()
     .isLength({ min: 1, max: 25 })
     .isString()
     .isAlpha()
     .escape(),
-  body('char.stats', 'validation failed: stats').isObject().trim().escape(),
+  body('recruit.recruitee.stats', 'validation failed: stats').isObject(),
+  body('recruit.recruitee.stats.strenght', 'validation failed: strenght')
+    .trim()
+    .not()
+    .isEmpty()
+    .isNumeric()
+    .escape(),
+  body('recruit.recruitee.stats.strenght', 'validation failed: strenght')
+    .trim()
+    .not()
+    .isEmpty()
+    .isNumeric()
+    .escape(),
+  body('recruit.recruitee.stats.dexterity', 'validation failed: dexterity')
+    .trim()
+    .not()
+    .isEmpty()
+    .isNumeric()
+    .escape(),
+  body(
+    'recruit.recruitee.stats.constitution',
+    'validation failed: constitution',
+  )
+    .trim()
+    .not()
+    .isEmpty()
+    .isNumeric()
+    .escape(),
+  body(
+    'recruit.recruitee.stats.intelligence',
+    'validation failed: intelligence',
+  )
+    .trim()
+    .not()
+    .isEmpty()
+    .isNumeric()
+    .escape(),
+  body('recruit.recruitee.stats.willpower', 'validation failed: willpower')
+    .trim()
+    .not()
+    .isEmpty()
+    .isNumeric()
+    .escape(),
+  body('recruit.price', 'validation failed: price')
+    .trim()
+    .not()
+    .isEmpty()
+    .isNumeric()
+    .escape(),
 ];
